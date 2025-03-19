afebbccba8970a61b302c54331b952c975fc70fb
root <june.nahmgoong>
2025-03-19 11:37:58 +0000

    c

--- init_db.py
+++ init_db.py
@@ -23,8 +23,8 @@ def init_db():
     ''')
     
     # 초기 점수 113으로 설정
-    c.execute('INSERT INTO school_scores (school_name, score) VALUES (?, ?)', ('yonsei', 1878))
-    c.execute('INSERT INTO school_scores (school_name, score) VALUES (?, ?)', ('korea', 2337))
+    c.execute('INSERT INTO school_scores (school_name, score) VALUES (?, ?)', ('yonsei', 1893))
+    c.execute('INSERT INTO school_scores (school_name, score) VALUES (?, ?)', ('korea', 2441))
     
     conn.commit()
     conn.close()
